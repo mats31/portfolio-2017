2ce047f9dfbdb1cfbbdf00b6c2bcea1eca4a52b1
Mathis Biabiany <mathis@resn.co.nz>
2016-12-27 10:42:42 +0100

    First commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const Config = {
+  devtool: 'inline-source-map',
+  entry: './src/main.js',
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    root: path.resolve( __dirname, 'src' ),
+    extensions: [
+      '',
+      '.js',
+      '.vue',
+      '.json',
+      '.styl',
+    ],
+  },
+  module: {
+    postLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'ify',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /node_modules/,
+        loader: 'ify',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader',
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader!glslify-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /animation.gsap\.js$/,
+        loader: 'imports?define=>false',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: 'body',
+      template: 'src/template/index.tpl.html',
+    }),
+    new webpack.ProvidePlugin({
+      Vue: 'vue/dist/vue',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'static' },
+    ]),
+  ],
+};
+
+module.exports = Config;
